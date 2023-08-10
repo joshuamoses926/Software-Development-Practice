07632db4dcc8de9827f010d8d9eab168f0718177
Joshua Moses <79341086+joshuamoses926@users.noreply.github.com>
2023-08-10 18:24:53 -0500

    Delete Software Development directory

--- Software Development/1-Basic Syntax/Exercise 1.py
+++
@@ -1,7 +0,0 @@
-"""
-Is This Code Correct?
-Will the following program work? If not, can you spot and fix the error?
-"""
-#print("Hello Python!')
-
-print("Hello Python!")
--- Software Development/4-Loops/for-loops.py
+++
@@ -1,4 +0,0 @@
-for index in range(5):
-    # print("Called from within a for-in loop")
-    if index % 2 == 0: continue
-    print(f"{index} - iteration count {index + 1}")
